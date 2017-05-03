1ea382ce5873acffe95b054211c724c5570492ab
gbhrdt <gbhrdt@users.noreply.github.com>
2017-05-03 10:50:04 +0200

    initial commit

---
+++ git-status.sh
@@ -0,0 +1,14 @@
+RED='\033[0;31m'
+NC='\033[0m'
+
+for D in `ls -d -1 $PWD/**/`
+do
+  cd "$D"
+  if [ -d ".git" ]; then
+    if [[ `git status --porcelain` ]]; then
+      printf "${RED}LOCAL CHANGES FOUND:$NC $D\n"
+    fi
+  fi
+  cd ".."
+done
+
