dbde71e5bd823a8fb89562ece173c66521b070a2
Marius <gbhrdt@users.noreply.github.com>
2017-05-04 01:08:39 +0200

    Ignore untracked files

--- git-status.sh
+++ git-status.sh
@@ -5,7 +5,7 @@ for D in `ls -d -1 $PWD/**/`
 do
   cd "$D"
   if [ -d ".git" ]; then
-    if [[ `git status --porcelain` ]]; then
+    if [[ `git status --porcelain -uno` ]]; then
       printf "${RED}LOCAL CHANGES FOUND:$NC $D\n"
     fi
   fi
